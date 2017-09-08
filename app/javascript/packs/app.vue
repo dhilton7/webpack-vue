485debe01dcfa340ca496c416e44f5a3b3149e56
Dan Hilton <dhilton@capterra.com>
2017-09-07 23:13:40 -0400

    change nav bar to always be full width

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,50 +1,61 @@
 <template>
-  <v-app id="example-1" toolbar footer>
+  <v-app
+    height="415px"
+    id="e3"
+    standalone
+    toolbar
+  >
     <v-navigation-drawer
       persistent
-      v-model="drawer"
-      light
+      absolute
+      height="100%"
+      clipped
       enable-resize-watcher
-      overflow
+      v-model="drawer"
     >
-    <v-list>
-      <v-list-tile @click="">
-        <v-list-tile-action>
-          <v-icon>home</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <router-link to="/"><v-list-tile-title>Home</v-list-tile-title></router-link>
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile @click="">
-        <v-list-tile-action>
-          <v-icon>list</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <router-link to="/properties"><v-list-tile-title>Properties</v-list-tile-title></router-link>
-        </v-list-tile-content>
-      </v-list-tile>
+    <v-list dense>
+      <v-list-tile v-for="item in items">
+      <v-list-tile-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
     </v-list>
   </v-navigation-drawer>
-  <v-toolbar class="indigo" dark>
+<v-toolbar class="red">
+  <v-toolbar-title>
     <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-  <v-toolbar-title>Toolbar</v-toolbar-title>
+  <v-icon class="ml-3">fa-youtube</v-icon>
+</v-toolbar-title>
+<v-spacer></v-spacer>
 </v-toolbar>
 <main>
-  <v-container fluid>
-    <!--v-router-->
-    <router-view></router-view>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <router-view></router-view>
+      </v-flex>
+    </v-layout>
   </v-container>
 </main>
 </v-app>
 </template>
 
 <script>
-export default {
-  data () {
+  export default {
+  data() {
     return {
-      drawer: true
+      drawer: true,
+      items: [
+        { name: 'Home', icon: 'home' },
+        { name: 'Properties', icon: 'list' }
+      ]
     }
   }
 }
 </script>
+
+<style>
+</style>
