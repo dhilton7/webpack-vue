df8da85f627039cf768fb0b19e1ef414b4bc3043
Dan Hilton <dhilton@capterra.com>
2017-09-09 11:22:27 -0400

    code clean up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-  skip_before_action :require_no_authentication
-
   clear_respond_to
   respond_to :json
 
-  def create
-    u = User.create(user_params)
-    if u.save
-      render json: { success: true }, status: 201
-    else
-      render json: { errors: 'meow' }, status: 422
-    end
-  end
+  protected
 
-  private
-
-  def user_params
+  def sign_up_params
     params.require(:registration).permit(:email, :password, :password_confirmation)
   end
 end
--- app/javascript/components/authentication/Signup.vue
+++ app/javascript/components/authentication/Signup.vue
@@ -25,16 +25,36 @@
         class="mt-3"
       ></v-text-field>
     </v-card-text>
+    <v-card-actions>
+      <v-btn medium success @click="signUp">Sign Me Up!</v-btn>
+    </v-card-actions>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     data() {
       return {
         email: '',
         password: '',
         password_confirmation: ''
+      }
+    },
+    methods: {
+      signUp() {
+        axios.post('/users', {
+          email: this.email,
+          password: this.password,
+          password_confirmation: this.password_confirmation
+        })
+          .then(response => {
+            console.log(response)
+          })
+          .catch(error => {
+            console.log(error)
+          })
       }
     }
   }
