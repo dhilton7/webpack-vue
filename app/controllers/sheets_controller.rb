44059e4216038df480e6d9c2f2fa805a812a17d1
Dan Hilton <dhilton@capterra.com>
2017-09-16 15:49:31 -0400

    add sheets controller

---
+++ app/controllers/sheets_controller.rb
@@ -0,0 +1,35 @@
+class SheetsController < ApplicationController
+  before_action :set_sheet, only: %i[show update destroy]
+
+  def create
+    @sheet = current_user.sheets.new sheet_params
+    if @sheet.save
+      render json: { success: true }, status: :created
+    else
+      render json: { error: @sheet.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @sheets = current_user.sheets.order('year desc, title asc')
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_sheet
+    @sheet = Sheet.find params[:id]
+  end
+
+  def sheet_params
+    params.require(:sheet).permit(:title, :year)
+  end
+end
